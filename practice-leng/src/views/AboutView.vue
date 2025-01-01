5d9e461acbe64530b0b5d115de1908e9ddc29472
Rotha Sam <samrotha98@gmail.com>
2025-01-01 08:49:22 +0700

    add to new folder

---
+++ practice-leng/src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+    <Navbar />
+
+    <main>
+        <h1 class="text-center">
+            This is About Page
+        </h1>
+    </main>
+
+</template>
+
+<script setup>
+import Navbar from '@/components/Navbar.vue'
+</script>
