134a4a4a677a1f2d50aba19148cd2027db1c31f3
Rotha Sam <samrotha98@gmail.com>
2025-01-04 22:13:20 +0700

    new homework contact-book

---
+++ 03_Contact-Book/src/views/ContactBookView.vue
@@ -0,0 +1,9 @@
+<template>
+    <main class="min-vh-100">
+        <ContactBook/>
+    </main>
+</template>
+
+<script setup>
+import ContactBook from '@/components/contact-book/ContactBook.vue'
+</script>
