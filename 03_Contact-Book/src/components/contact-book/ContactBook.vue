87193f78f2a9e34021102eaa9f2a5ce4013c0993
Rotha Sam <samrotha98@gmail.com>
2025-01-07 21:08:10 +0700

    done add contact function

---
+++ 03_Contact-Book/src/components/contact-book/ContactBook.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container">
+            <div class="row">
+                <div class="col-5 mx-auto content-wrap">
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div>
+                            <h3>Contact Book</h3>
+                            <p>Save your friend's contact.</p>
+                        </div>
+                        <div>
+                            <a role="button" 
+                            @click="showAddModal()" 
+                            class="fs-4" ><i class="iconoir-plus-circle-solid"></i></a>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <input type="text" class="form-control" placeholder="Search contact" />
+                    </div>
+                    <ul class="list-group">
+                        <li 
+                            v-for="contact in contactManage.contacts" 
+                            class="list-group-item contact-info">
+
+                            <div class="d-flex justify-content-start">
+                                <span class="cover">
+                                    {{ contact.firstName[0] + contact.lastName[0]}}
+                                </span>
+                                <div class="ms-3">
+                                    <p class="mb-0 fw-bold fs-5">
+                                        {{ contact.firstName + " " + contact.lastName }}
+                                    </p>
+                                    <span class="d-flex justify-content-start align-items-center">
+                                        <i class="iconoir-phone"></i>
+                                        <span class="ms-2">
+                                            {{ contact.phone }}
+                                        </span>
+                                    </span>
+                                </div>
+                            </div>
+                            <div>
+                                <a 
+                                    @click="contactManage.deleteContact(contact.id)"
+                                    role="button" class="text-danger fs-5"><i class="iconoir-trash"></i>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+        </div>
+</template>
+<script setup>
+
+import { useContactStore } from '@/stores/store';
+const contactManage = useContactStore();
+
+
+const showAddModal = () => {
+    contactManage.modal.show();
+}
+
+
+
+</script>
--- 03_Contact-Book/src/stores/store.js
+++ 03_Contact-Book/src/stores/store.js
@@ -1,34 +1,35 @@
 import { defineStore } from 'pinia';
 
-export const useAppStore = defineStore('store.js',{
+export const useContactStore = defineStore('store.js',{
     state: () => ({
-        mdlAdd: null,
+        modal: null,
         contacts: [
             {
                 id: 1,
-                name: 'Rotha',
+                firstName: 'Rotha',
+                lastName: 'haha',
                 phone: '012444444'
             },
             {
                 id: 2,
-                name: 'Sam',
+                firstName: 'Rotha',
+                lastName: 'haha',
                 phone: '09988776'
-            },
-            {
-                id: 3,
-                name: 'Kon pov',
-                phone: '01234567'
             }
         ],
+        seleted_id: 0
+        
     }),
 
     actions: {
-        // addContact({ commit }, contact) {
-        //     contact.id = this.contacts.length + 1;
-        //     commit('addContact', contact);
-        // },
-        // deleteContact({ commit }, contactId) {
-        //     commit('deleteContact', contactId);
+        // deleteContact: (seletedID) => {
+        //     console.log(seletedID);
+        //     this.seleted_id = seletedID;
         // }
+        deleteContact(selectedID) {
+            console.log(selectedID); // Log the passed selected ID
+            this.selected_id = selectedID; // Correctly set the selected ID in the store
+            this.modal.show();
+        },
     }
 })
